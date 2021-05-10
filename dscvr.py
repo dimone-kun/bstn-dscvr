a899aef1f329054b4c89d46917d88dee568b87c3
Dmitry Chigileychik <chigileychik@gmail.com>
2021-05-11 01:15:10 +0500

    Типизация для данных

--- dscvr.py
+++ dscvr.py
@@ -1,6 +1,11 @@
 import typing
+import typing_extensions
 import socket
 import sys
+import json
+
+
+Host = typing_extensions.TypedDict("Host", {"name": str, "address": str, "ports": typing.List[int]})
 
 
 def scan_host_ports(sock: typing.Tuple[str, int]):
@@ -15,6 +20,6 @@ if __name__ == '__main__':
     hosts_file_path = sys.argv[1]
     with open(hosts_file_path, "r") as hosts_file:
         hosts = json.load(hosts_file)
-        for host in hosts:
+        for host in hosts:  # type: Host
             for port in host['ports']:
                 scan_host_ports((host['address'], port))
